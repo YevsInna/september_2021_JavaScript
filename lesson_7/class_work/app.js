19a026419ce078ef75003189422ce58b28ca9eb4
YevsInna <evseinka@gmail.com>
2021-11-27 19:07:54 +0200

    lesson_7_class_work

--- lesson_7/class_work/app.js
+++ lesson_7/class_work/app.js
@@ -49,7 +49,7 @@ console.log(car);
 // -- increaseMaxSpeed (newSpeed) - яка підвищує значення максимальної швидкості на значення newSpeed
 // -- changeYear (newValue) - змінює рік випуску на значення newValue
 // -- addDriver (driver) - приймає об'єкт який "водій" з довільним набором полів, і додає його в поточний об'єкт car
-class Car{
+class Car2{
       constructor(model, producer, year, maxSpeed, engine) {
         this.model = model;
         this.producer = producer;
@@ -75,11 +75,52 @@ class Car{
           };
     };
 };
-let car2 = new Car('Q5', 'Audi ', 2020, 190, 2);
-
+let car2 = new Car('Q5', 'Audi ', 2020, 190, 1.8);
+car2.drive();
+car2.info();
+car2.increaseMaxSpeed(80);
+car2.changeYear(2021);
+car2.addDriver({name:'Max', experience: 10})
+console.log(car2);
 
 
 // -створити класс/функцію конструктор попелюшка з полями ім'я, вік, розмір ноги. Створити масив з 10 попелюшок.
+class Popelushka{
+    constructor(name, age,footSize){
+        this.name = name;
+        this.age = age;
+        this.footSize = footSize;
+    };
+};
+let popelushki = [];
+popelushki.push(new Popelushka('Liza',20, 37),
+                new Popelushka('Olya',16, 36),
+                new Popelushka('Nadya',15, 33),
+                new Popelushka('Marysya',21, 38),
+                new Popelushka('Dasha',19, 36),
+                new Popelushka('Lena',22, 35),
+                new Popelushka('Bohdana',11, 34),
+                new Popelushka('Larisa',62, 36),
+                new Popelushka('Tamara',29, 42),
+                new Popelushka('Kristina',30, 39),
+)
+console.log(popelushki);
 // Сторити об'єкт класу "принц" за допомоги класу який має поля ім'я, вік, туфелька яку він знайшов.
+class Prince{
+    constructor(name,age,shoes){
+        this.name = name;
+        this.age = age;
+        this.shoes = shoes;
+    };
+};
+let prince = new Prince('William', 35, 39)
+console.log(prince);
 //     За допомоги циклу знайти яка попелюшка повинна бути з принцом.
+for(let item of popelushki){
+    if(item.footSize === prince.shoes){
+        console.log(`З принцом ${prince.name} повинна бути попелюшка ${item.name}`);
+    };
+};
 //     Додатково, знайти необхідну попелюшку за допомоги функції масиву find та відповідного колбеку
+const popelushka = popelushki.find((value) => value.footSize === prince.shoes);
+console.log(popelushka);
