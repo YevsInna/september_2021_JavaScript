95bcbfaf2d7353667c98d638dba76cf7c26f9f61
YevsInna <evseinka@gmail.com>
2021-12-05 16:48:19 +0200

    lesson_10_home_work

---
+++ lesson_10/app.js
@@ -0,0 +1,31 @@
+// - Создать произвольный елемент с id = text.  Используя JavaScript, сделайте так,
+// чтобы при клике на кнопку исчезал элемент с id="text".
+// let text = document.getElementById('text');
+// let btn = document.createElement('button');
+// btn.innerText = 'to make text invisible'
+// document.body.appendChild(btn);
+//
+// btn.onclick = function (){
+//     text.style.display = 'none'
+// };
+
+//- Создайте кнопку, при клике на которую, она будет скрывать сама себя.
+// let btn2 = document.createElement('button');
+// btn2.innerText = 'press me'
+// document.body.appendChild(btn2);
+//
+// btn2.onclick = function (){
+//     btn2.style.display = 'none'
+// };
+
+
+// - створити інпут який приймає вік людини та кнопку яка підтверджує дію.При натисканні на кнопку зчитати інформацію з інпуту
+// та перевірити вік чи меньше він ніж 18, та повідомити про це користувача
+//
+// - Создайте меню, которое раскрывается/сворачивается при клике
+//
+//
+// - Создать список комментариев , пример объекта коментария - {title : 'lorem', body:'lorem ipsum dolo sit ameti'}.
+//     Вывести список комментариев в документ, каждый в своем блоке.
+//     Добавьте каждому комментарию по кнопке для сворачивания его body.
+
