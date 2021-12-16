682b50f14bace3da5c778c3709e4ee0c9979581d
YevsInna <evseinka@gmail.com>
2021-12-16 20:54:48 +0200

    lesson_13_additional

--- lesson_13/app.js
+++ lesson_13/app.js
@@ -34,3 +34,4 @@ goods.onsubmit = function (e){
 
 
 
+
--- lesson_13/list.js
+++ lesson_13/list.js
@@ -4,44 +4,39 @@ let goods = JSON.parse(goodsFromLS);
 // console.log(goods);
 
 for (const good of goods) {
-  let divElement = document.createElement('div');
-  divElement.classList.add('card');
-  let name = document.createElement('h3');
-  name.innerText = `${good.name}`;
-  let count = document.createElement('h3');
-  count.innerText = `Кількість товару - ${good.count}`;
-  let picture = document.createElement('img');
-  picture.src = `${good.picture}`;
+    let divElement = document.createElement('div');
+    divElement.classList.add('card');
+    let name = document.createElement('h3');
+    name.innerText = `${good.name}`;
+    let count = document.createElement('h3');
+    count.innerText = `Кількість товару - ${good.count}`;
+    let picture = document.createElement('img');
+    picture.src = `${good.picture}`;
 
-  let deleteBtn = document.createElement('button');
-  deleteBtn.innerText = 'Видалити товар';
+    let deleteBtn = document.createElement('button');
+    deleteBtn.innerText = 'Видалити товар';
 
-  let deleteItem = (id, arr) => {
-      for (let i = 0; i < arr.length; i++) {
-        if (arr[i].id === id) {
-          arr.splice(i, 1)
-        };
-      };
+    deleteBtn.onclick = function (){
+      const index = goods.indexOf(good);
+      goods.splice(index,1);
+      localStorage.setItem('goods', JSON.stringify(goods));
+      location.reload();
     };
-  deleteBtn.addEventListener('click', () => {
-    deleteItem (good.id, goods);
-      localStorage.setItem('goods',JSON.stringify(goods))
-    })
 
-  wrap.appendChild(divElement);
-  divElement.appendChild(name);
-  divElement.appendChild(count);
-  divElement.appendChild(picture);
-  divElement.appendChild(deleteBtn);
+    wrap.appendChild(divElement);
+    divElement.appendChild(name);
+    divElement.appendChild(count);
+    divElement.appendChild(picture);
+    divElement.appendChild(deleteBtn);
 };
 
 let btnDelete = document.createElement('button');
 btnDelete.innerText = 'Видалити все';
 document.body.appendChild(btnDelete);
 
-btnDelete.onclick = function (){
-  localStorage.removeItem('goods');
-  };
+btnDelete.onclick = function () {
+    localStorage.removeItem('goods');
+};
 
 
 
